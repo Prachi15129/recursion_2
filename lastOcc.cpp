f60d7317bc2e4dfe3fd007436cbebaeb1ac62091
Prachi15129 <67423563+Prachi15129@users.noreply.github.com>
2021-04-20 23:11:49 +0530

    Create lastOcc.cpp

---
+++ lastOcc.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int lastOcc(int arr[],int n,int key)
+{
+    if(n==0)
+    {
+        return -1;
+    }
+    int i=lastOcc(arr+1,n-1,key);
+    if(i==-1)
+    {
+        if(arr[0]==key)
+        {
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+       
+    }
+     return i+1;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    cout<<lastOcc(arr,n,key);
+    return 0;
+}
